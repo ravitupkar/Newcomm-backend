f7c7dfebab5627da5d3aaa1b962ad20f5b5b1e89
ravitupkar <ravitupkar01@gmail.com>
2019-11-11 15:37:39 +0530

    curd product

--- routes/products.js
+++ routes/products.js
@@ -37,4 +37,47 @@ router.post('/create', function(req, res, next) {
   
 });
 
+router.get('/edit/:id', function(req, res, next) {
+  Products.findOne({_id : req.params.id})
+  .then(result => {
+    res.json(result);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+})
+
+router.post('/update', function(req, res, next) {
+  Products.findOneAndUpdate({_id : req.body._id}, {
+    $set : req.body
+  })
+  .then(result => {
+    res.json(result);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+})
+
+router.get('/delete/:id', function(req, res, next) {
+  Products.findOne({_id : req.params.id})
+  .then(result => {
+    if(result.length > 0){
+      Products.findOneAndRemove({_id : req.params.id})
+          .then(result => {
+            res.json(result);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+    }else{
+      res.json({"error": "Porduct Not Found"});
+    }
+          
+  })
+  .catch(err => {
+    res.json(err);
+  });
+})
+
 module.exports = router;
